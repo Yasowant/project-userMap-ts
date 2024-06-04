1968690a3216f52b3ea6f88d66081aa952da4ece
Yasowant <yasowant1998@gmail.com>
2024-06-04 08:23:44 +0530

    end of the project

--- src/CustomMaps.ts
+++ src/CustomMaps.ts
@@ -1,3 +1,15 @@
+//<reference types="@types/google.maps" />
+// Instructions to every other class
+// on how they can be an argument to 'addMarker'
+export interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+  markerContent(): string;
+  color: string;
+}
+
 export class CustomMap {
   private googleMap: google.maps.Map;
 
@@ -12,5 +24,23 @@ export class CustomMap {
         },
       }
     );
+  }
+
+  addMarker(mappable: Mappable): void {
+    const marker = new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng,
+      },
+    });
+
+    marker.addListener('click', () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: mappable.markerContent(),
+      });
+
+      infoWindow.open(this.googleMap, marker);
+    });
   }
 }
